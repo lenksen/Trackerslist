8f53451059072807695c05b3faa069fb50a9fcdf
Zhijie He <hezhijie0327@hotmail.com>
2021-02-26 17:08:36 +0800

    release.sh | Version 1.0.2
    1. <Change> Add new source from "notracking/hosts-blocklists-scripts".

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.0.1
+# Current Version: 1.0.2
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/Trackerslist.git" && bash ./Trackerslist/release.sh
@@ -10,6 +10,8 @@
 function GetData() {
     dead_domain=(
         "https://raw.githubusercontent.com/hezhijie0327/DHDb/main/dhdb_dead.txt"
+        "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt"
+        "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.dead.txt"
     )
     trackerlist_combine=(
         "http://www.torrenttrackerlist.com/torrent-tracker-list"
